e0b92cc355fa4f1baf37f20e3877805b13b21db3
oylx <oylx2008@163.com>
2019-04-23 15:15:37 +0800

    安装jest-junit,配置jest.config,生成详细的测试覆盖率

--- lib/icons/icon.example.tsx
+++ lib/icons/icon.example.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Icon from './icon';
+import Icon from './../icon/icon';
 const IconExample:React.FunctionComponent = ()=>{
     return(
         <div>
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Icon from './icons/icon'
+import Icon from './icon/icon'
 
 const fn:React.MouseEventHandler =(e)=>{
     console.log(e)
