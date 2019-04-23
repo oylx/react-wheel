dc82792f03afa7ce28034106f79cb41c45c06624
oylx <oylx2008@163.com>
2019-04-23 14:35:51 +0800

    object-mock.js解决scss测试方法

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
         },
     },
     moduleNameMapper: {
-        "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+        // "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+        "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",//解决scss测试问题法2
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
     },
     testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
