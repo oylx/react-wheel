4d4cd0f99931787f3c86de1fe6d0b88886411fca
oylx <oylx2008@163.com>
2019-04-23 14:07:29 +0800

    使用snapshot测试UI，测试点击事件

--- lib/icon/__tests__/icon.unit.tsx
+++ lib/icon/__tests__/icon.unit.tsx
@@ -1,10 +1,18 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
 import Icon from './../icon'
+import {mount} from 'enzyme'
 
 describe('icon', () => {
     it('xxx', () => {
         const json = renderer.create(<Icon name='wechat'/>).toJSON()
         expect(json).toMatchSnapshot()
+    })
+    it('onClick',()=>{
+        let n = 1;
+        const fn = ()=>{n=2};
+        const component = mount(<Icon name="water" onClick={fn}/>)
+        component.find('svg').simulate('click')
+        expect(n).toEqual(2)
     })
 })
