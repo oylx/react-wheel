fec460d630d9ed6b0f6efc95361e55a0bfec14e1
oylx <oylx2008@163.com>
2019-04-23 11:11:16 +0800

    jest的moduleNameMapper配置identity-obj-proxy，解决scss引入测试

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,7 @@ module.exports = {
         },
     },
     moduleNameMapper: {
+        "^.+\\.(css|less|scss)$": "identity-obj-proxy",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
     },
     testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
