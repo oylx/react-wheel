b6ad0a03a37d2fa39c202d73fcb9f110d7f02085
oylx <oylx2008@163.com>
2019-04-23 09:47:22 +0800

    让icon响应所有事件{...props}

---
+++ lib/icons/icon.example.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Icon from './icon';
+const IconExample:React.FunctionComponent = ()=>{
+    return(
+        <div>
+            <Icon name="qq"/>
+            <Icon name="wechat"/>
+            <Icon name="water"/>
+        </div>
+    )
+}
+export default  IconExample
