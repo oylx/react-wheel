617e025b6d59149225df177eed3008cf330f50fa
oylx <oylx2008@163.com>
2019-09-06 14:31:49 +0800

    add modal

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,18 +1,37 @@
 import React, {useState} from 'react'
-import Dialog from './dialog'
+import Dialog,{alert} from './dialog'
 
 export default function () {
   const [x, setX] = useState(false)
+  const [y, setY] = useState(false)
   return (
     <div>
-      <button onClick={() => setX(!x)}>点我</button>
-      <Dialog visible={x}
-              onClose={() => setX(false)}
-              buttons={[
-                <button>1</button>,
-                <button>2</button>
-              ]}>
-      </Dialog>
+      <div style={{position: 'relative', border: 'solid 1px red', zIndex: 10, color: 'red'}}>
+        <h1>example 1</h1>
+        <button onClick={() => setX(!x)}>点我</button>
+        <Dialog visible={x}
+                onClose={() => setX(false)}
+                buttons={[
+                  <button>1</button>,
+                  <button>2</button>
+                ]}>
+        </Dialog>
+      </div>
+      <div>
+        <h1>example 2</h1>
+        <button onClick={() => setY(!y)}>点我</button>
+        <Dialog visible={y}
+                onClose={() => setY(false)}
+                buttons={[
+                  <button>1</button>,
+                  <button>2</button>
+                ]}>
+        </Dialog>
+      </div>
+      <div>
+        <h1>example 3</h1>
+        <button onClick={() => alert('1')}>alert</button>
+      </div>
     </div>
   )
 }
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,48 +1,66 @@
 import React, {Fragment, ReactElement} from 'react';
+import ReactDOM from 'react-dom'
 import {Icon} from '../index'
 import {scopedClassMaker} from '../classes'
 import "./dialog.scss"
 import "./../index.scss"
 
 interface Props {
-  visible:boolean;
-  buttons:Array<ReactElement>;
-  onClose:React.MouseEventHandler;
-  closeOnclickMask?:boolean
+  visible: boolean;
+  buttons?: Array<ReactElement>;
+  onClose: React.MouseEventHandler;
+  closeOnclickMask?: boolean
 }
 
 const scopedClass = scopedClassMaker('fui-dialog')
 const sc = scopedClass
 console.log(sc)
 
-const Dialog:React.FunctionComponent<Props>=(props)=>{
-  const onClickClose:React.MouseEventHandler =(e)=>{
+const Dialog: React.FunctionComponent<Props> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
     props.onClose(e)
   }
-  const onClickMask:React.MouseEventHandler =(e)=>{
-    if(props.closeOnclickMask){
+  const onClickMask: React.MouseEventHandler = (e) => {
+    if (props.closeOnclickMask) {
       props.onClose(e)
     }
   }
-  return (
-    props.visible?//return必须是组件或null,但是props.children可能是组件或非组件，所以包一层div
-      <Fragment>
-        <div className={sc('mask')} onClick={onClickMask}></div>
-        <div className={sc()}>
-          <div className={sc('close')} onClick={onClickClose}>
-            <Icon name="close"></Icon>
-          </div>
-          <header className={sc('header')}>提示</header>
-          <main className={sc('main')}>
-            {props.children}
-          </main>
-          <footer className={sc('footer')}>
-            {props.buttons.map((button,index)=>React.cloneElement(button,{key:index}))}
-          </footer>
+  const x = props.visible ?//return必须是组件或null,但是props.children可能是组件或非组件，所以包一层div
+    <Fragment>
+      <div className={sc('mask')} onClick={onClickMask}></div>
+      <div className={sc()}>
+        <div className={sc('close')} onClick={onClickClose}>
+          <Icon name="close"></Icon>
         </div>
-      </Fragment>
-      :
-      null
+        <header className={sc('header')}>提示</header>
+        <main className={sc('main')}>
+          {props.children}
+        </main>
+        <footer className={sc('footer')}>
+          {props.buttons && props.buttons.map((button, index) => React.cloneElement(button, {key: index}))}
+        </footer>
+      </div>
+    </Fragment>
+    :
+    null
+  return (
+    ReactDOM.createPortal(x, document.body)
   )
 }
+Dialog.defaultProps = {
+  closeOnclickMask: false
+}
+const alert = (content: string) => {
+  const component = <Dialog visible={true} onClose={() => {
+    //关闭事件:重新渲染
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    //unmountComponentAtNode并remove元素
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+  }}>{content}</Dialog>;
+  const div = document.createElement('div');
+  document.body.appendChild(div)
+  ReactDOM.render(component, div)
+}
+export {alert}
 export default Dialog
