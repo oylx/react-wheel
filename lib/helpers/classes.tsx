b705fde427fcab87c8acf6fc6fbb25568aa00465
oylx <oylx2008@163.com>
2019-09-10 19:10:44 +0800

    函数式scopedClassMaker

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -1,29 +1,28 @@
-function classes(...names:(string | undefined)[]) {
-    return names.filter((names)=>names).join(' ')
+function classes(...names: (string | undefined)[]) {
+  return names.filter((names) => names).join(' ')
 }
+
 export default classes;
 
-interface classToggles {
-    [k: string]: boolean
+interface Options {
+  extra: string | undefined
 }
 
-function scopedClassMaker(prefix: string) {
 
-    return function (name?: string | classToggles, options?: { extra: string | undefined }) {
-        let result
-        if (typeof name === 'string' || name === undefined) {
-            result = [prefix, name].filter(Boolean).join('-');
-        } else {
-            // name ={hasAside:true,x:false,y:true}
-            const names = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0])
-            result = names.map(n => [prefix, n].filter(Boolean).join('-')).join(' ')
-        }
-        if (options && options.extra) {
-            return [result, options && options.extra].filter(Boolean).join(' ');
-        } else {
-            return result
-        }
-    }
+interface ClassToggles {
+  [k: string]: boolean
 }
+
+const scopedClassMaker= (prefix: string) =>
+  (name: string | ClassToggles, options?: Options) =>
+  Object
+    .entries(name instanceof Object ? name : {[name]: name})
+    .filter(kv => kv[1] !== false)
+    .map(kv => kv[0])
+    .map(name => [prefix, name]
+      .filter(Boolean)
+      .join('-'))
+    .concat(options && options.extra || [])
+    .join(' ')
 
 export {scopedClassMaker}
