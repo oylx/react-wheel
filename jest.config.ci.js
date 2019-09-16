0fdb78851ece15a032accf757acfd7e86cd9885d
oylx <oylx2008@163.com>
2019-09-16 16:04:35 +0800

    remove file

--- jest.config.ci.js
+++
@@ -1,9 +0,0 @@
-// https://jestjs.io/docs/en/configuration.html
-const base = require("./jest.config")
-module.exports = Object.assign({},base,{
-    collectCoverage: true,
-    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-    reporters: ["default"],
-})
