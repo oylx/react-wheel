e70d37cbd906541bc1b99afbfa146912de97d101
oylx <oylx2008@163.com>
2019-09-18 19:59:06 +0800

    受控组件概念

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,26 +1,42 @@
-import React,{Fragment,useState} from 'react'
-import Form from './form'
+import React, {useRef, useState} from 'react'
+// import Form from './form'
 
 const FormExample: React.FunctionComponent = () => {
-  const [formData] = useState({
-    username:'',
-    password:''
-  })
+  // const [formData] = useState({
+  //   username: '',
+  //   password: ''
+  // })
+  //
+  // const [fields] = useState([
+  //   {name: 'username', label: '用户名', input: {type: 'text'}},
+  //   {name: 'password', label: '密码', input: {type: 'password'}}
+  // ])
 
-  const [fields]=useState([
-    {name:'username',label:'用户名',input:{type:'text'}},
-    {name:'password',label:'密码',input:{type:'password'}}
-  ])
+  const [name, setName] = useState('frank');
+  const refInput = useRef<HTMLInputElement>(null);
 
+  const x = () => {
+    console.log(refInput.current!.value || '')//refInput.current!.value断言
+  }
   return (
-   <Fragment>
-     <Form value={formData} fields={fields}
-           buttons={<Fragment>
-             <button type="submit">提交</button>
-             <button>返回</button>
-           </Fragment>}>
-     </Form>
-   </Fragment>
+    <>
+      //受控组件,受react控制
+      <input value={name} onChange={(e) => {setName(e.target.value)}}/>
+      //非受控组件,不受react控制
+      <input defaultValue={name} ref={refInput} type="text" onBlur={x}/>
+    </>
   )
+  // return (
+  //   <>
+  //     <Form value={formData}
+  //           fields={fields}
+  //           onSubmit={() => {}}
+  //           buttons={<>
+  //             <button type="submit">提交</button>
+  //             <button>返回</button>
+  //           </>}>
+  //     </Form>
+  //   </>
+  // )
 }
 export default FormExample
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -4,7 +4,7 @@ interface Props {
   value: { [k: string]: any },
   fields: Array<{ name: string, label: string, input: { type: string } }>,
   buttons: ReactFragment,
-  // onSubmit: React.FormEventHandler
+  onSubmit: React.FormEventHandler
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
